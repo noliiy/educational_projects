68ebeca546268765edaff6a3f315d4ceee7df142
Emre Erdem <133537591+noliiy@users.noreply.github.com>
2023-08-09 15:23:13 +0300

    Add files via upload

---
+++ Quizzler-iOS13/Quizzler-iOS13/Model/Question.swift
@@ -0,0 +1,21 @@
+//
+//  Question.swift
+//  Quizzler-iOS13
+//
+//  Created by Emre Erdem on 21.07.2023.
+//  Copyright © 2023 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct Question {
+    let text: String
+    let answer: String
+    
+    init(q: String, a: String) {
+        text = q
+        answer = a
+    }
+}
+
+
