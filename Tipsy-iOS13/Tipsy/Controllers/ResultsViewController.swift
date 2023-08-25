c077b0d38048994788016f95b5bc3699b4080724
Emre Erdem <133537591+noliiy@users.noreply.github.com>
2023-08-25 19:22:05 +0300

    Add files via upload

---
+++ Tipsy-iOS13/Tipsy/Controllers/ResultsViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ResultsViewController.swift
+//  Tipsy
+//
+//  Created by Emre Erdem on 24.08.2023.
+//  Copyright © 2023 The App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ResultsViewController: UIViewController {
+
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+}
